4503cd4c62cf80eef956616b05b81e69ad81e3b5
VladimirMozgin <mozgin.vova@gmail.com>
2017-04-02 18:27:11 +0300

    route resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331160701) do
+ActiveRecord::Schema.define(version: 20170402145936) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "trains", force: :cascade do |t|
