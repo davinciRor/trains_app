fdc567be2d99547af57d934d4ef53e47b0d72cdd
VladimirMozgin <mozgin.vova@gmail.com>
2017-04-13 17:19:48 +0300

    add validates

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,4 +4,19 @@ class Route < ActiveRecord::Base
   has_many :railway_stations, through: :railway_stations_routes
 
   validates :name, presence: true
+  validates :stations_count
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
+
+  def stations_count
+    if railway_stations.сount < 2
+      errors.add(:base, 'Route must have minimum 2 station')
+    end
+  end
 end
