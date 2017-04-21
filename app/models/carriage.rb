a3479ed14483a8880370dd10a1a5be8d5482abc2
VladimirMozgin <mozgin.vova@gmail.com>
2017-04-21 12:17:53 +0300

    fix bug

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,22 +1,18 @@
 class Carriage < ActiveRecord::Base
+  TYPES = %w(CoupeCarriage CVCarriage EconomCarriage SeatCarriage)
+
   belongs_to :train
 
   validates :train, :type, :number, presence: true
-  validates :number, uniqueness: true
+  validates :number, uniqueness: { scope: :train_id }
 
   before_validation :add_number
 
   def add_number
-    self.number ||= Carriage.max_number.to_i + 1
+    self.number ||= self.max_number + 1
   end
 
-  class << self
-    def types
-      %w(CoupeCarriage CVCarriage EconomCarriage SeatCarriage)
-    end
-
-    def max_number
-      pluck(:number).max || 0
-    end
+  def max_number
+    train.carriages.pluck(:number).max || 0
   end
 end
